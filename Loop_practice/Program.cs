667f5940bf923e094c5b61e7a00a585e21ff8d03
Bryce <ImperialEagle1998@gmail.com>
2020-10-04 14:22:31 -0500

    Added a extra comment for clarity about the bottom

--- Loop_practice/Program.cs
+++ Loop_practice/Program.cs
@@ -70,7 +70,7 @@ namespace Loop_practice
             {
                "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
             };
-
+            // I know these are foreach loops, but I justw anted to try something out. :P
             foreach(string letter in alphabet)
             {
                 Console.WriteLine(letter);
